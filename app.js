5794d852cf3298ffadec59227d18398b3f31b2cd
Alexandra Milea <alexandramilea@Alexandras-MacBook-Pro.local>
2022-11-03 10:23:52 +0100

    Update code

--- app.js
+++ app.js
@@ -6,8 +6,13 @@ const keys = document.querySelectorAll('.key');
 const whiteKeys = document.querySelectorAll('.key.white');
 const blackKeys = document.querySelectorAll('.key.black');
 
+
+
+var randomNotePressed = []
+
 // Added an event listener of each key you press 
 keys.forEach(key => {
+    console.log(key)
     key.addEventListener('click', () => playNote(key));
 })
 
@@ -23,8 +28,9 @@ document.addEventListener('keydown', e => {
 
 // Main play sound for notes function.
 function playNote(key) {
-
+    console.log(key.dataset.note)
     const noteAudio = document.getElementById(key.dataset.note);
+    console.log("noteAudio", noteAudio)
     noteAudio.currentTime = 0;
     noteAudio.play();
     // Css class for animation 
@@ -32,4 +38,42 @@ function playNote(key) {
     noteAudio.addEventListener('ended', () => {
         key.classList.remove('active');
     })
+}
+
+function play(){
+    var i = 0
+    const notes = localStorage.getItem("NOTE_PRESSED").split(",")
+    const allNotes = ["A", "Ab", "B", "Bb"]
+    if(notes.length === 0){
+        setTimeout(function(){
+            const randomIndex = Math.floor(Math.random()* allNotes.length)
+            const n = allNotes[randomIndex]
+            randomNotePressed.push(n)
+            localStorage.setItem("NOTE_PRESSED" , randomNotePressed)
+            const rightAnswerValue = localStorage.getItem("NOTE_PRESSED")
+            document.getElementById('rightanswer').innerHTML= rightAnswerValue
+            //document.getElementById("wronganswer1").innerHTML = rightAnswerValue
+            //document.getElementById("wronganswer2").innerHTML = rightAnswerValue
+            const noteAudio = document.getElementById(n);
+            noteAudio.currentTime = 0;
+            noteAudio.play();
+                }, 1000*i)
+    }else {
+        for(i; i<notes.length;i++){
+            setTimeout(function(){
+                const randomIndex = Math.floor(Math.random()* allNotes.length)
+                const n = allNotes[randomIndex]
+                randomNotePressed.push(n)
+                localStorage.setItem("NOTE_PRESSED" , randomNotePressed)
+                const rightAnswerValue = localStorage.getItem("NOTE_PRESSED")
+                document.getElementById('rightanswer').innerHTML= rightAnswerValue
+                //document.getElementById("wronganswer1").innerHTML = rightAnswerValue
+                //document.getElementById("wronganswer2").innerHTML = rightAnswerValue
+                const noteAudio = document.getElementById(n);
+                noteAudio.currentTime = 0;
+                noteAudio.play();
+                    }, 1000*i)
+                }
+    }
+    
 }
